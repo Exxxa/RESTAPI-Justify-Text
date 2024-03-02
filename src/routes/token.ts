27e6656ec0eb469d16037be1a9337e2a8d678c9e
LOPEZ PIERRE <lopez@et.esiea.fr>
2024-03-02 10:27:13 +0100

    changing method and using mongodb database

--- src/routes/token.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import tokenController from '../controllers/tokenController';
-
-const router = express.Router();
-
-router.post('/api/token', tokenController);
-
-export default router;
