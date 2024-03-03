660330b00e5a6b844ce345947673e291183ad305
LOPEZ PIERRE <lopez@et.esiea.fr>
2024-03-03 18:20:08 +0100

    added working online mode

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,33 @@
-import express from 'express'; // Import the Express framework
-import http from 'http'; // Import the built-in HTTP module
-import bodyParser from 'body-parser'; // Import middleware for parsing request bodies
-import cookieParser from 'cookie-parser'; // Import middleware for parsing cookies
-import compression from 'compression'; // Import middleware for HTTP compression
-import cors from 'cors'; // Import middleware for enabling CORS (Cross-Origin Resource Sharing)
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
 
-import router from './router'; // Import router module
-import mongoose from 'mongoose'; // Import Mongoose library for MongoDB interactions
+import router from './router';
+import mongoose from 'mongoose';
 
-const app = express(); // Create an Express application
+const app = express();
 
-app.use(cors({ // Enable CORS middleware with options
-  credentials: true, // Allow sending credentials
+app.use(cors({
+  credentials: true,
 }));
 
-app.use(compression()); // Use compression middleware for HTTP compression
-app.use(cookieParser()); // Use cookie-parser middleware for parsing cookies
-app.use(bodyParser.json()); // Use body-parser middleware for parsing JSON request bodies
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
-const server = http.createServer(app); // Create an HTTP server using the Express app
+const server = http.createServer(app);
 
-server.listen(34567, () => { // Start the server and listen on port 34567
-  console.log('Server running on http://myPersonalIP/'); // Log the server URL
+server.listen(34567, () => {
+  console.log('Server running on http://91.170.250.88:34567/');
 });
 
-const MONGO_URL = 'mongodb+srv://lopez:zrHWVTiNuwbyGge9@clusterinternship.ltbeauf.mongodb.net/?retryWrites=true&w=majority&appName=clusterinternship'; // MongoDB URI
+const MONGO_URL = 'mongodb+srv://pierre:HF7lmPHbxqxMr5qp@clusterinternship.ltbeauf.mongodb.net/?retryWrites=true&w=majority&appName=clusterinternship'; // DB URI
 
-mongoose.Promise = Promise; // Set Mongoose's promise library to the built-in JavaScript Promise
-mongoose.connect(MONGO_URL); // Connect to MongoDB using the provided URI
-mongoose.connection.on('error', (error: Error) => console.log(error)); // Log any errors that occur during the MongoDB connection
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
 
-app.use('/', router()); // Use the router module to handle requests to the root URL ('/')
-
+app.use('/', router());
